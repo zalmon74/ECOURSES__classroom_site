a1104496887060b592093439dcc401b2d1d3c96b
zalmon74 <zalmon74@yandex.ru>
2023-03-31 13:41:19 +0700

    create app accounts

--- ECOURSES__classroom/config/settings.py
+++ ECOURSES__classroom/config/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
