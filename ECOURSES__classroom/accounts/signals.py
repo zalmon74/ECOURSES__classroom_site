48cb4b80076345244422e469577dbb30f8bd5b3a
zalmon74 <zalmon74@yandex.ru>
2023-04-08 18:19:05 +0700

    group name changed in datebase

--- ECOURSES__classroom/accounts/signals.py
+++ ECOURSES__classroom/accounts/signals.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import Group
 def create_default_all_groups(sender, *args, **kwargs):
     """ Создание стандартных групп пользователей
     """
-    list_group = [Group(name=_) for _ in settings.ALL_GROUPS.values()]
+    list_group = [Group(name=_) for _ in settings.ALL_GROUPS.keys()]
     Group.objects.bulk_create(list_group)
     
 
